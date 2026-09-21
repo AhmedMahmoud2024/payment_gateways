b0c47ec274178317a2fb0480d9de6dd4edf7286a
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-09-21 02:36:42 -0700

    feat:implemented mock_wallet_repository

---
+++ lib/features/checkout/data/repositories/mock_wallet_repository.dart
@@ -0,0 +1,26 @@
+import 'package:payment_gateways/features/checkout/data/models/wallet_input_model.dart';
+import 'package:payment_gateways/features/checkout/data/repositories/mock_checkout_repository.dart';
+
+class MockWalletOTPRequired extends MockPaymentResult{
+  final String phoneNumber;
+  MockWalletOTPRequired(this.phoneNumber);
+
+  Future<MockPaymentResult> processWalletPayment(WalletInputModel wallet)async{
+ await Future.delayed(const Duration(seconds: 2));
+ final cleanPhone= wallet.phoneNumber;
+ 
+ if(cleanPhone.endsWith('0000')){
+  return MockWalletOTPRequired(phoneNumber);
+ }
+ return MockSuccess('WAL_${DateTime.now().millisecondsSinceEpoch}');
+  }
+
+  Future<MockPaymentResult> verifyOtp(String code)async{
+ await Future.delayed(const Duration(seconds: 1));
+
+if(code =='1234'){
+  return MockSuccess('WAL_${DateTime.now().millisecondsSinceEpoch}');
+}
+return MockFailure('OTP is incorrect,try again');
+  }
+}
