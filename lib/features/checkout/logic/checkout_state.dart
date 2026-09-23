85481068814906c4a463ea2d9d170a58cb64ab1b
Ahmed Mahmoud <ahmedmahmoudsaleh269@gmail.com>
2026-09-23 16:21:16 -0700

    feat:refactor checkout_state

--- lib/features/checkout/logic/checkout_state.dart
+++ lib/features/checkout/logic/checkout_state.dart
@@ -1,43 +1,53 @@
-import 'package:meta/meta.dart';
 
-@immutable
-sealed class CheckoutState {
+import 'package:equatable/equatable.dart';
+
+sealed class CheckoutState extends Equatable{
   const CheckoutState();
+  @override
+  List<Object?> get props=>[];
 }
 
 // 1. الحالة المبدئية: الشاشة لسه مفرغة
-class CheckoutInitial extends CheckoutState {
+final class CheckoutInitial extends CheckoutState {
   const CheckoutInitial();
 }
 
 // 2. حالة التحميل: جاري التحدث مع الـ Server وضبط الـ UI للانتظار
-class CheckoutLoading extends CheckoutState {
+final class CheckoutLoading extends CheckoutState {
   const CheckoutLoading();
 }
 
 // 3. حالة النجاح: العملية تمت وبنرجع رقم الشحنة/المعاملة
-class CheckoutSuccess extends CheckoutState {
+final class CheckoutSuccess extends CheckoutState {
   final String transactionId;
   const CheckoutSuccess(this.transactionId);
+  @override
+  List<Object?> get props => [transactionId];
 }
 
 // 4. حالة التوجيه لصفحة تأكيد البنك (3DS)
-class Checkout3DSRequired extends CheckoutState {
+final class Checkout3DSRequired extends CheckoutState {
   final String redirectUrl;
   const Checkout3DSRequired(this.redirectUrl);
+@override
+  List<Object?> get props => [redirectUrl];
 }
 
 // 5. حالة الفشل: حدث خطأ في الـ Validation أو السيرفر مرفوض
-class CheckoutFailure extends CheckoutState {
+final class CheckoutFailure extends CheckoutState {
   final String errorMessage;
   const CheckoutFailure(this.errorMessage);
+ @override
+  List<Object?> get props => [errorMessage];
 }
-class CheckoutOTPRequired extends CheckoutState {
+final class CheckoutOTPRequired extends CheckoutState {
   final String phoneNumber;
   const CheckoutOTPRequired(this.phoneNumber);
+  @override
+  List<Object?> get props => [phoneNumber];
 }
 
-class CheckoutFawryCodeGenerated extends CheckoutState{
+final class CheckoutFawryCodeGenerated extends CheckoutState{
   final String referenceNumber;
   final String expireTime;
 
